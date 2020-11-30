3ceb4b5a1bfe4154678bd9a073871005845d485e
Fanil <FanilBiktimirov@yandex.ru>
2020-12-01 00:09:48 +0500

    1.1.3 Реализовал методы интерфейса UserService в UserServiceImpl

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,29 @@
 package jm.task.core.jdbc.util;
+import java.sql.*;
+import com.mysql.cj.jdbc.Driver;
 
 public class Util {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/mytest?serverTimezone=Europe/Moscow&useSSL=false";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "Afybkm06061998";
     // реализуйте настройку соеденения с БД
+
+    public static Connection getConnection() {
+
+        Connection connection = null;
+        try {
+            Driver driver = new Driver();
+            DriverManager.registerDriver(driver);
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            //Statement statement = connection.createStatement();
+            if (!connection.isClosed()) {
+                System.out.println("Соединение с БД установлено");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.err.println("Error");
+        }
+        return connection;
+    }
 }
---
+++ src/main/resources/test.sql
@@ -0,0 +1,4 @@
+#insert into users (name, lastName, age) values('Fanil','Biktimirov', 22);
+#select * from users;
+#update users set name = "Alex", age = 19 where id = 1;
+#delete from users where id >= 9;
