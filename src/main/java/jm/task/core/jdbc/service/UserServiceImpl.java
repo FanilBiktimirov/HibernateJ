6d2e300f6863f58109ab7a53f8f9f449e1ce3e71
Fanil <FanilBiktimirov@yandex.ru>
2020-11-30 23:43:19 +0500

    git push

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,31 +1,91 @@
 package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
-public class UserServiceImpl implements UserService {
-    public void createUsersTable() {
+public class UserServiceImpl extends Util implements UserService {
 
+    Connection connection = getConnection();
+
+    public void createUsersTable() {
+        String createUsersTable = "CREATE TABLE IF NOT EXISTS users (id INTEGER AUTO_INCREMENT PRIMARY KEY, \n" +
+                "    firstName VARCHAR(30), \n" +
+                "    lastName VARCHAR(30),\n" +
+                "    age INTEGER\n" +
+                ");";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(createUsersTable)) {
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void dropUsersTable() {
-
+        String drop = "DROP TABLE IF EXISTS users";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(drop)) {
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        String saveUser = "INSERT INTO users (firstName, lastName, age) Values (?, ?, ?)";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(saveUser)) {
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setInt(3, age);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void removeUserById(long id) {
-
+        try {
+            String removeUserById = "DELETE FROM users WHERE id = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(removeUserById);
+            preparedStatement.setLong(1, id);
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> userList = new ArrayList<>();
+        try {
+            String getAllUsers = "SELECT * FROM users;";
+
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(getAllUsers);
+
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getLong(1));
+                user.setName(resultSet.getString(2));
+                user.setLastName(resultSet.getString(3));
+                user.setAge(resultSet.getByte(4));
+
+                userList.add(user);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return userList;
     }
 
     public void cleanUsersTable() {
-
+        String cleanUsersTable = "TRUNCATE TABLE users";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(cleanUsersTable);
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
